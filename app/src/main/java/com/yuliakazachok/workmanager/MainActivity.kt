44e7378bb5e0b74a6e1ef4dcab32e7b5dd70ddaf
Yulia Kazachok <yuliakazachok@gmail.com>
2023-11-12 12:24:03 +0700

    Initial commit

---
+++ app/src/main/java/com/yuliakazachok/workmanager/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.yuliakazachok.workmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.yuliakazachok.workmanager.ui.theme.WorkManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WorkManagerTheme {
+            }
+        }
+    }
+}
